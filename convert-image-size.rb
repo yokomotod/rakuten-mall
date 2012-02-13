5077282814e4d178743f0cf1a4d9f5ddb6b30640
Daisuke Yokomoto <yokomotod@gmail.com>
2012-02-13 23:46:22 +0900

    convert image size to regular square
    WebGL required regular square size image for texture.
    So we have to convert crawled image to reqular square size.

---
+++ convert-image-size.rb
@@ -0,0 +1,20 @@
+require 'rubygems'
+require 'RMagick'
+
+image_dir = 'image/'
+
+Dir::entries(image_dir).each do |filename|
+  next if filename == '.' || filename == '..'
+
+  orig = Magick::Image.read(image_dir + filename).first
+
+  width = orig.columns
+  height = orig.rows
+
+  size = width > height ? width : height
+
+  orig.background_color = 'white'
+  image = orig.extent(size, size, (size-width)/2, (size-height)/2)
+
+  image.write(image_dir + 'r_' + filename)
+end
